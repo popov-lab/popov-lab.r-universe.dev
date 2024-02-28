ac2dd6046bfb52ad325945d1fd112a95ccac3cdc
Ven Popov <vencislav.popov@gmail.com>
2024-02-28 03:42:10 +0100

    why does r-universe keep failing to build chkptstanr?

--- make_json.R
+++ make_json.R
@@ -1,6 +1,6 @@
 df <- data.frame(
     package = c("bmm","chkptstanr"),
     url = c("https://github.com/venpopov/bmm", "https://github.com/venpopov/chkptstanr"),
-    branch = c("*release","*release")
+    branch = c("*release","v0.2.0")
 )
 jsonlite::write_json(df, 'packages.json', pretty = TRUE)
