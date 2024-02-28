bef90d9bd08f5f21f2a1651ec4da7f908e0734ef
Ven Popov <vencislav.popov@gmail.com>
2024-02-28 02:13:17 +0100

    update branch source

--- make_json.R
+++ make_json.R
@@ -1,6 +1,6 @@
 df <- data.frame(
     package = c("bmm"),
     url = c("https://github.com/venpopov/bmm", "https://github.com/venpopov/chkptstanr"),
-    branch = c("*release","*release")
+    branch = c("@*release","@*release")
 )
 jsonlite::write_json(df, 'packages.json', pretty = TRUE)
