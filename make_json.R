24e7528ecc7cea2f0f1348ceb7bc0827b5c8ebee
Ven Popov <vencislav.popov@gmail.com>
2024-02-28 04:03:22 +0100

    will it update everything if I add a new package?

--- make_json.R
+++ make_json.R
@@ -1,6 +1,6 @@
 df <- data.frame(
-    package = c("bmm","chkptstanr"),
-    url = c("https://github.com/venpopov/bmm", "https://github.com/venpopov/chkptstanr"),
-    branch = c("*release","v0.2.0")
+    package = c("bmm","chkptstanr","Vmisc"),
+    url = c("https://github.com/venpopov/bmm", "https://github.com/venpopov/chkptstanr", "https://github.com/venpopov/Vmisc"),
+    branch = c("*release","v0.2.0", "master")
 )
 jsonlite::write_json(df, 'packages.json', pretty = TRUE)
