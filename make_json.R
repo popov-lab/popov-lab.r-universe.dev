0a34530234aed5b2235e3c90c5b7e18ec8cd1b30
Ven Popov <vencislav.popov@gmail.com>
2024-02-28 01:53:49 +0100

    update to track bmm master branch

--- make_json.R
+++ make_json.R
@@ -1,5 +1,6 @@
 df <- data.frame(
     package = c("bmm"),
-    url = c("https://github.com/venpopov/bmm")
+    url = c("https://github.com/venpopov/bmm"),
+    branch = c("master")
 )
 jsonlite::write_json(df, 'packages.json', pretty = TRUE)
