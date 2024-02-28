798e4bcb6a0b330af7a660f5d5e95dcc3addbdb1
Ven Popov <vencislav.popov@gmail.com>
2024-02-28 04:17:11 +0100

    explicitely add v0.4.0 of bmm

--- make_json.R
+++ make_json.R
@@ -1,6 +1,6 @@
 df <- data.frame(
-    package = c("bmm","chkptstanr","Vmisc"),
-    url = c("https://github.com/venpopov/bmm", "https://github.com/venpopov/chkptstanr", "https://github.com/venpopov/Vmisc"),
-    branch = c("*release","v0.2.0", "master")
+    package = c("bmm","bmm","chkptstanr","Vmisc"),
+    url = c("https://github.com/venpopov/bmm", "https://github.com/venpopov/bmm", "https://github.com/venpopov/chkptstanr", "https://github.com/venpopov/Vmisc"),
+    branch = c("*release","v0.4.0","v0.2.0", "master")
 )
 jsonlite::write_json(df, 'packages.json', pretty = TRUE)
